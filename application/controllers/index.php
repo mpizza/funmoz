524715b54adaa472569427ceddc7b9f7a606bdb0
mpizza <mmm198219@gmail.com>
2012-12-07 18:19:07 +0800

    fixed index

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -6,17 +6,7 @@ class Index extends CI_Controller {
   }
   
 	public function index(){
-	  $this->load->library('Layout');
-		$data['title'] = 'title';
-		$data['heading'] = 'heading text';
-		$db = $this->load->database();
-		$sql = 'SELECT * FROM imagedata WHERE id = ?';
-    $query = $this->db->query($sql, array(1)); 
-    $row = $query->row_array(0);
-		$data['content'] = $row['dataurl'];
-		
 		$this->layout->view('index_page', $data);
-		//$this->load->view('blog_message');
 	}
 
   private function _utility(){  
