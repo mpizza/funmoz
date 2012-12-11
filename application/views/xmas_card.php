68758ff5e9fde9581495290043ef5e24dc3dc766
mpizza <mmm198219@gmail.com>
2012-12-12 00:27:00 +0800

    add new title and add fb, gplus script

--- application/views/xmas_card.php
+++ application/views/xmas_card.php
@@ -37,7 +37,26 @@
   <input type="text" value="<?php echo $cards_url; ?>" id="c_url">
   </section>
 </div>
+<!-- Place this tag after the last +1 button tag. -->
+<script type="text/javascript">
+  window.___gcfg = {lang: 'zh-TW'};
+
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/zh_TW/all.js#xfbml=1&appId=266112973491035";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
 <section class="social_content">
-  <h2>留言</h2>
+  <h2>留言給創作者</h2>
   <div class="fb-comments" data-href="<?php echo $cards_url; ?>" data-width="950" data-num-posts="20" data-colorscheme="dark"></div>
 </section>
