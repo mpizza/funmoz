2216541b6fdee2d64bb6bce12dcbea11b810813d
mpizza <mmm198219@gmail.com>
2012-12-12 00:20:22 +0800

    remove social js`

--- application/views/xmas_card.php
+++ application/views/xmas_card.php
@@ -4,16 +4,6 @@
     <h2>我愛網路，我願意與Firefox一同守護網路開放自由</h2>
   </div>
 </hgroup>
-<!-- Place this tag after the last +1 button tag. -->
-<script type="text/javascript">
-  window.___gcfg = {lang: 'zh-TW'};
-
-  (function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/plusone.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  })();
-</script>
 <div id="main-content">
   <section>
     <?php echo $content;?>
@@ -47,16 +37,6 @@
   <input type="text" value="<?php echo $cards_url; ?>" id="c_url">
   </section>
 </div>
-<div id="fb-root"></div>
-<script>
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/zh_TW/all.js#xfbml=1&appId=266112973491035";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-</script>
 <section class="social_content">
   <h2>留言</h2>
   <div class="fb-comments" data-href="<?php echo $cards_url; ?>" data-width="950" data-num-posts="20" data-colorscheme="dark"></div>
