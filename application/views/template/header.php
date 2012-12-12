cda32c4df7406649410417eb310824d20aed1f3a
mpizza <mmm198219@gmail.com>
2012-12-12 14:13:41 +0800

    add touch event and viewport

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -11,7 +11,7 @@
 <!DOCTYPE html>
 <html xml:lang="zh-TW" lang="zh-TW">
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
   <meta http-equiv="content-type" content="text/html; charset=UTF-8"/> 
   <meta name="description" content="<?php echo $meta_desc; ?>" />
   <meta name="keywords" content="<?php echo $keyword;?>" />
